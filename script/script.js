0e12bb71d9ac129ef5ed4bf5e89761b0c272421c
dimosoff <dimmiostr@mail.ru>
2022-12-15 15:32:33 +0300

    beta (pred prod)

--- script/script.js
+++ script/script.js
@@ -1,7 +1,23 @@
 const preloaderElement = document.querySelector(".preloader");
 const popupElement = document.querySelector(".popup");
+const popupWishElement = document.querySelector(".popup-wish");
 const popupContent = document.querySelector(".popup__content");
+const presents = document.querySelectorAll(".present-hover");
+const finishButton = document.getElementById("finish");
+const confettiElement = document.querySelector(".confetti");
+const logoElement = document.querySelector(".logo");
 let pageHeight = window.innerHeight;
+
+const popupMessages = {
+  m1: "В 2023 году самые амбициозные планы воплотятся в жизнь, а удача будет сопутствовать во всех начинаниях!",
+  m2: "Наступающий год будет годом финансовой стабильности, свежих идей и новых свершений!",
+  m3: "Наступающий год подарит гармонию в душе и впечатляющие суммы на банковском счете!",
+  m4: "В наступающем году исполнятся заветные мечты: вас ждут новые победы, яркие впечатления и море улыбок!",
+  m5: "В 2023 году работа будет приносить удовольствие, а ваши труды будут вознаграждены по достоинству (например, отпуском на райских островах)!",
+  wrongOrientation: "Измените ориентацию вашего устройства.",
+  noticeToChoose: "Выбери подарок и узнай предсказание на 2023 год",
+};
+
 // eslint-disable-next-line no-unused-vars
 function afterLoad() {
   document.addEventListener("resize", orientationHandler());
@@ -9,22 +25,40 @@ function afterLoad() {
 
   console.log("loaded");
   preloaderElement.classList.add("hidden");
+
   setTimeout(() => {
     document.querySelector(".app").classList.add("animate");
-  }, 2000)
+  }, 2000);
+  setTimeout(() => {
+    showPopup(popupMessages.noticeToChoose);
+  }, 11000);
+
+  presents.forEach((present) =>
+    present.addEventListener("click", (event) => {
+      const messageCode = event.currentTarget.dataset.message;
+      hidePopup();
+      logoElement.classList.add("hidden");
+      showPopup(popupMessages[messageCode], popupWishElement);
+    })
+  );
+
+  finishButton.addEventListener("click", () => {
+    confettiElement.classList.add("active");
+    popupWishElement.classList.add("finished");
+  });
 }
 
 function orientationHandler() {
   if (window.innerWidth < window.innerHeight) {
-    showPopup("Измените ориентацию вашего устройства.");
+    showPopup(popupMessages.wrongOrientation);
     return false;
   }
   hidePopup();
 }
-function showPopup(message = "") {
-  popupElement.classList.add("active");
-  popupContent.textContent = message;
+function showPopup(message = "", popup = popupElement) {
+  popup.classList.add("active");
+  popup.querySelector(".popup__content").textContent = message;
 }
-function hidePopup() {
-  popupElement.classList.remove("active");
+function hidePopup(popup = popupElement) {
+  popup.classList.remove("active");
 }
